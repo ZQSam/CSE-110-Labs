614ebb1447bbd91c6f3058616aa20b44b5f1a3e7
ZQSam <131108692+ZQSam@users.noreply.github.com>
2024-10-24 15:08:40 -0700

    exercise 2 done

--- Lab 4/src/components/Remaining.tsx
+++ Lab 4/src/components/Remaining.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Remaining = () => {
@@ -9,9 +9,16 @@ const Remaining = () => {
     return (total = total + item.cost);
   }, 0);
 
+  const remain = budget - totalExpenses;
+
   const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
 
   // Exercise: Create an alert when Remaining is less than 0.
+  useEffect(() => {
+    if (remain < 0) {
+      alert("You have exceeded your budget!");
+    }
+  }, [remain]);
 
   return (
     <div className={`alert ${alertType}`}>
